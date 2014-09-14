e2c06d7c49e853a096cabdf3de83c41a3c40f995
Leonardo Lessa <lrlessa@gmail.com>
2014-09-14 00:01:48 -0300

    create gruntile and front-end structure

---
+++ app/webroot/Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+	var jsFiles = [
+		'js/src/app.js'
+	];
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			dev: {
+				options: {
+					banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				},
+				files: {
+					'js/script.min.js': [jsFiles]
+				}
+			}
+		},
+		stylus: {
+			dev: {
+				options: {
+					paths: ['./css/stylus'],
+					compress: false,
+					use: [require('nib')]
+				},
+				files: {
+					'./css/styles.css': './css/stylus/styles.styl'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: ['css/stylus/**/*.styl'],
+				tasks: ['stylus'],
+				options: {
+					livereload: true
+				}
+			},
+			js: {
+				files: ['js/src/**/*.js'],
+				tasks: ['uglify'],
+				options: {
+					livereload: true
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['uglify', 'stylus', 'watch']);
+
+}
